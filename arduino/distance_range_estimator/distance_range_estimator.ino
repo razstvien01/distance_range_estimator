4d057b80947690c003225b48876c963b0789e7a1
Nico Aricayos <nicolenevanz.aricayos@gmail.com>
2023-12-04 12:36:46 +0800

    update: main.dart and dre.ino code

--- arduino/distance_range_estimator/distance_range_estimator.ino
+++ arduino/distance_range_estimator/distance_range_estimator.ino
@@ -1,45 +1,70 @@
 #include <ESP8266WiFi.h>
 #include <PubSubClient.h>
 
+// Update these with your network credentials
 const char* ssid = "HG8145V5_0484D";
 const char* password = "PZz6VKmu";
-const char* mqttServer = "test.mosquitto.org";
-const int mqttPort = 1883;
-const char* mqttTopic = "distance_range_estimator";
+
+// MQTT Broker
+const char* mqtt_broker = "broker.hivemq.com";
+const char* topic = "distance_range_estimator";
+const int mqtt_port = 1883;
 
 WiFiClient espClient;
 PubSubClient client(espClient);
 
-void setup() {
-  Serial.begin(115200);
-  WiFi.begin(ssid, password);
-  while (WiFi.status() != WL_CONNECTED) {
-    delay(1000);
+// Declare the variables
+unsigned long lastMsg = 0; // To store the time when the last message was sent
+long temperature;          // To store the temperature value
+char msg[50];              // To store the message to be sent
+
+void setup_wifi() {
+    delay(10);
+    // Connect to Wi-Fi
     Serial.println("Connecting to WiFi...");
-  }
-  client.setServer(mqttServer, mqttPort);
+    WiFi.begin(ssid, password);
+
+    while (WiFi.status() != WL_CONNECTED) {
+        delay(500);
+        Serial.print(".");
+    }
+
+    Serial.println("WiFi connected");
+    Serial.println("IP address: ");
+    Serial.println(WiFi.localIP());
+}
+
+void setup() {
+    Serial.begin(115200);
+    setup_wifi();
+    client.setServer(mqtt_broker, mqtt_port);
 }
 
 void loop() {
-  if (!client.connected()) {
-    reconnect();
-  }
-  client.loop();
-  // Send data to Flutter app
-  client.publish(mqttTopic, "Hello Nicolen from ESP8266");
-  delay(5000);
-}
-
-void reconnect() {
-  while (!client.connected()) {
-    Serial.println("Connecting to MQTT...");
-    if (client.connect("ESP8266Client")) {
-      Serial.println("Connected to MQTT");
-    } else {
-      Serial.println("Failed, rc=");
-      Serial.println(client.state());
-      Serial.println(" Retrying in 5 seconds...");
-      delay(5000);
+    if (!client.connected()) {
+        // Reconnect if connection is lost
+        while (!client.connected()) {
+            Serial.print("Attempting MQTT connection...");
+            // Attempt to connect
+            if (client.connect("ESP8266Client")) {
+                Serial.println("connected");
+            } else {
+                Serial.print("failed, rc=");
+                Serial.print(client.state());
+                Serial.println(" try again in 5 seconds");
+                delay(5000);
+            }
+        }
     }
-  }
+    client.loop();
+
+    // Publish a message roughly every two seconds
+    if (millis() - lastMsg > 2000) {
+        lastMsg = millis();
+        temperature = random(20, 30); // Generate a random temperature value
+        snprintf(msg, 50, "Temperature: %ld", temperature);
+        Serial.print("Publish message: ");
+        Serial.println(msg);
+        client.publish(topic, msg);
+    }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,147 +1,98 @@
-import 'dart:math';
 import 'package:flutter/material.dart';
-import 'package:mqtt_client/mqtt_client.dart';
-import 'package:mqtt_client/mqtt_server_client.dart';
+import 'package:mqtt_client/mqtt_client.dart' as mqtt;
+import 'package:mqtt_client/mqtt_server_client.dart' as mqtt;
 
-String generateClientId() {
-  return 'FlutterClient_${Random().nextInt(1000)}';
-}
-
-void main() async {
-  final MqttServerClient client = MqttServerClient('test.mosquitto.org', generateClientId());
-  
-  client.logging(on: true);
-  client.keepAlivePeriod = 20;
-  
-  await client.connect();
-  
-  client.subscribe("distance_range_estimator", MqttQos.atMostOnce);
-  
-  client.updates?.listen((List<MqttReceivedMessage<MqttMessage>> c) {
-    final MqttPublishMessage recMess = c[0].payload as MqttPublishMessage;
-    final String message = MqttPublishPayload.bytesToStringAsString(recMess.payload.message);
-    print('Received message: $message');
-  });
-  
-  runApp(const MyApp());
-}
+void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Flutter MQTT Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
+        primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: MyHomePage(),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  _MyHomePageState createState() => _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+  final mqtt.MqttServerClient client = mqtt.MqttServerClient('broker.hivemq.com', '');
+  String receivedMessage = 'Waiting for messages...';
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
+  @override
+  void initState() {
+    super.initState();
+    connect();
+  }
+
+  void connect() async {
+    client.setProtocolV311();
+    client.logging(on: true);
+    client.keepAlivePeriod = 30;
+    client.onDisconnected = onDisconnected;
+    client.onConnected = onConnected;
+    client.onSubscribed = onSubscribed;
+
+    try {
+      await client.connect();
+    } catch (e) {
+      print('Exception: $e');
+      client.disconnect();
+    }
+
+    if (client.connectionStatus?.state == mqtt.MqttConnectionState.connected) {
+      print('MQTT client connected');
+    } else {
+      print('ERROR MQTT client connection failed - disconnecting, status is ${client.connectionStatus}');
+      client.disconnect();
+    }
+  }
+
+  void onConnected() {
+    print('Connected');
+    client.subscribe("distance_range_estimator", mqtt.MqttQos.atMostOnce);
+  }
+
+  void onDisconnected() {
+    print('Disconnected');
+  }
+
+  void onSubscribed(String topic) {
+    print('Subscribed to topic: $topic');
+    client.updates?.listen((List<mqtt.MqttReceivedMessage<mqtt.MqttMessage>> c) {
+      final mqtt.MqttPublishMessage message = c[0].payload as mqtt.MqttPublishMessage;
+      final payload = mqtt.MqttPublishPayload.bytesToStringAsString(message.payload.message);
+      
+
+      print('Received message: $payload from topic: ${c[0].topic}>');
+      setState(() {
+        receivedMessage = payload;
+      });
     });
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+        title: Text("MQTT Demo"),
       ),
       body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
-        ),
+        child: Text(receivedMessage),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
+  }
+
+  @override
+  void dispose() {
+    client.disconnect();
+    super.dispose();
   }
 }
