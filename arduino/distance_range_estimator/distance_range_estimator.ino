2380778b867c4c02a5bcee1ed8063f0e70a2a975
Nico Aricayos <nicolenevanz.aricayos@gmail.com>
2023-12-04 17:06:49 +0800

    update: dre.ino

--- arduino/distance_range_estimator/distance_range_estimator.ino
+++ arduino/distance_range_estimator/distance_range_estimator.ino
@@ -1,7 +1,7 @@
 #include <ESP8266WiFi.h>
 #include <PubSubClient.h>
 
-// Update these with your network credentials
+// Network credentials
 const char* ssid = "HG8145V5_0484D";
 const char* password = "PZz6VKmu";
 
@@ -14,57 +14,75 @@ WiFiClient espClient;
 PubSubClient client(espClient);
 
 // Declare the variables
-unsigned long lastMsg = 0; // To store the time when the last message was sent
-long distance;          // To store the distance value
-char msg[50];              // To store the message to be sent
+unsigned long lastMsg = 0;
+long distance;
+char msg[50];
+
+// Button setup
+const int buttonPin = D2;  // Change D3 to your ESP8266 GPIO pin connected to the button
+bool lastButtonState = LOW;
+int ledSendPin = D1;
+int ledConnectionPin = D5;
 
 void setup_wifi() {
-    delay(10);
-    // Connect to Wi-Fi
-    Serial.println("Connecting to WiFi...");
-    WiFi.begin(ssid, password);
-
-    while (WiFi.status() != WL_CONNECTED) {
-        delay(500);
-        Serial.print(".");
-    }
-
-    Serial.println("WiFi connected");
-    Serial.println("IP address: ");
-    Serial.println(WiFi.localIP());
+  // Connect to Wi-Fi
+  Serial.println("Connecting to WiFi...");
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  pinMode(ledSendPin, OUTPUT);
+  pinMode(ledConnectionPin, OUTPUT);
+  Serial.println("WiFi connected");
+  Serial.println("IP address: ");
+  Serial.println(WiFi.localIP());
 }
 
 void setup() {
-    Serial.begin(115200);
-    setup_wifi();
-    client.setServer(mqtt_broker, mqtt_port);
+  Serial.begin(115200);
+  setup_wifi();
+  client.setServer(mqtt_broker, mqtt_port);
+
+  pinMode(buttonPin, INPUT);  // Initialize the button pin as an input
 }
 
 void loop() {
-    if (!client.connected()) {
-        // Reconnect if connection is lost
-        while (!client.connected()) {
-            Serial.print("Attempting MQTT connection...");
-            // Attempt to connect
-            if (client.connect("ESP8266Client")) {
-                Serial.println("connected");
-            } else {
-                Serial.print("failed, rc=");
-                Serial.print(client.state());
-                Serial.println(" try again in 5 seconds");
-                delay(5000);
-            }
-        }
-    }
-    client.loop();
 
-    // Publish a message roughly every two seconds
-    if (millis() - lastMsg > 2000) {
-        lastMsg = millis();
-        distance = random(1, 400); // Generate a random distance value
-        snprintf(msg, 50, "Current Distance: %ld cm", distance);
-        Serial.print("Publish message: ");
-        Serial.println(msg);
-        client.publish(topic, msg);
+  if (!client.connected()) {
+    while (!client.connected()) {
+      digitalWrite(ledConnectionPin, LOW);
+      Serial.print("Attempting MQTT connection...");
+      if (client.connect("ESP8266Client")) {
+        digitalWrite(ledConnectionPin, HIGH);
+        Serial.println("connected");
+      } else {
+        Serial.print("failed, rc=");
+        Serial.print(client.state());
+        Serial.println(" try again in 5 seconds");
+        delay(5000);
+      }
     }
+  }
+
+  // client.loop();
+
+  bool buttonState = digitalRead(buttonPin);
+  if (buttonState == HIGH && lastButtonState == LOW && client.connected()) {
+
+    // Button is pressed
+    distance = random(1, 400);  // Generate a random distance value
+    snprintf(msg, 50, "Current Distance: %ld cm", distance);
+    Serial.print("Publish message: ");
+    Serial.println(msg);
+    client.publish(topic, msg);
+    lastMsg = millis();
+    digitalWrite(ledSendPin, HIGH);  // Turn the LED on
+    delay(50);                      // Wait for a second
+    digitalWrite(ledSendPin, LOW);   // Turn the LED off
+    delay(50);
+  }
+  lastButtonState = buttonState;  // Update the last button state
+
+  
 }
